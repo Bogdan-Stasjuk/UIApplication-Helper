1fae31cf6dbe082b2d6a8c74da9edd254106db39
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2015-06-15 01:51:14 +0300

    Initial Commit

---
+++ UIApplication+HelperDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIApplication+HelperDemo
+//
+//  Created by Bogdan Stasjuk on 6/15/15.
+//  Copyright © 2015 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIApplication+HelperDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIApplication+HelperDemo
+//
+//  Created by Bogdan Stasjuk on 6/15/15.
+//  Copyright © 2015 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
